c9a15b24047d1eb20d54297a721983bfd64f6df9
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2022-08-15 23:21:54 +0900

    add luasnip

---
+++ lua/lua-snip.lua
@@ -0,0 +1,11 @@
+local ls = require 'luasnip'
+local s = ls.s
+local sn = ls.sn
+local t = ls.t
+local i = ls.i
+local f = ls.f
+local c = ls.c
+local d = ls.d
+
+
+require("luasnip.loaders.from_lua").lazy_load({ include = { "lua" } })
