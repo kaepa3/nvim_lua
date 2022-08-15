4576a00bff9a7f4e6aa7449e790abaa4e186a3d3
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2022-08-16 04:19:02 +0900

    format

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,54 +1,54 @@
 vim.cmd [[packadd packer.nvim]]
 
 require 'packer'.startup(function()
-    use 'wbthomason/packer.nvim'
-    use 'tpope/vim-fugitive'
-    use 'tpope/vim-repeat'
-    --common
-    use { 'machakann/vim-sandwich' }
-    use {
-        "windwp/nvim-autopairs",
-        config = function() require("nvim-autopairs").setup {} end
+  use 'wbthomason/packer.nvim'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-repeat'
+  --common
+  use { 'machakann/vim-sandwich' }
+  use {
+    "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
+  use { 'kaepa3/swpclear' }
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  --style
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+  use "EdenEast/nightfox.nvim"
+  --lsp
+  use 'neovim/nvim-lspconfig'
+  use { 'williamboman/mason.nvim' }
+  use { 'williamboman/mason-lspconfig.nvim' }
+  use {
+    "williamboman/nvim-lsp-installer",
+  }
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      { 'L3MON4D3/LuaSnip' },
+      { 'saadparwaiz1/cmp_luasnip' }
     }
-    use { 'kaepa3/swpclear' }
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-    --style
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
-    use "EdenEast/nightfox.nvim"
-    --lsp
-    use 'neovim/nvim-lspconfig'
-    use { 'williamboman/mason.nvim' }
-    use { 'williamboman/mason-lspconfig.nvim' }
-    use {
-        "williamboman/nvim-lsp-installer",
-    }
-    use {
-        'hrsh7th/nvim-cmp',
-        requires = {
-            { 'L3MON4D3/LuaSnip' },
-            { 'saadparwaiz1/cmp_luasnip' }
-        }
-    }
-    use 'hrsh7th/cmp-nvim-lsp'
-    use{ 
-        'jose-elias-alvarez/null-ls.nvim', 
-        requires = 'nvim-lua/plenary.nvim' 
-    }
-    --html
-    use {
-        'mattn/emmet-vim',
-        ft = 'html',
-        opt = true
-    }
-    --markdown
-    use 'tyru/open-browser.vim'
-    use 'previm/previm'
+  }
+  use 'hrsh7th/cmp-nvim-lsp'
+  use {
+    'jose-elias-alvarez/null-ls.nvim',
+    requires = 'nvim-lua/plenary.nvim'
+  }
+  --html
+  use {
+    'mattn/emmet-vim',
+    ft = 'html',
+    opt = true
+  }
+  --markdown
+  use 'tyru/open-browser.vim'
+  use 'previm/previm'
 
 end)
 vim.cmd([[autocmd BufWritePost init.lua source <afile> | PackerCompile]])
@@ -61,66 +61,66 @@ require('lualine').setup()
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-lspconfig").setup_handlers {
-    function(server_name) -- default handler (optional)
-        require("lspconfig")[server_name].setup {
-            on_attach = on_attach
-        }
-    end,
+  function(server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup {
+      on_attach = on_attach
+    }
+  end,
 }
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 local lspconfig = require('lspconfig')
 lspconfig.cssls.setup {
-    capabilities = capabilities
+  capabilities = capabilities
 }
 lspconfig.html.setup {
-    capabilities = capabilities
+  capabilities = capabilities
 }
 
 local null_ls = require("null-ls")
 null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.prettier
-	},
+  sources = {
+    null_ls.builtins.formatting.prettier
+  },
 })
 
 -- 3. completion (hrsh7th/nvim-cmp)
 local cmp = require("cmp")
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-        end,
-    },
-    sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        -- { name = "path" },
-    },
-    mapping = cmp.mapping.preset.insert({
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ['<C-l>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm { select = true },
-    }),
-    experimental = {
-        ghost_text = true,
-    },
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    -- { name = "path" },
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ['<C-l>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+  }),
+  experimental = {
+    ghost_text = true,
+  },
 })
 function go_org_imports(wait_ms)
-    local params = vim.lsp.util.make_range_params()
-    params.context = { only = { "source.organizeImports" } }
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-    for _, res in pairs(result or {}) do
-        for _, r in pairs(res.result or {}) do
-            if r.edit then
-                vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
-            else
-                vim.lsp.buf.execute_command(r.command)
-            end
-        end
+  local params = vim.lsp.util.make_range_params()
+  params.context = { only = { "source.organizeImports" } }
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
     end
+  end
 end
 
 vim.cmd [[ autocmd BufWritePre *.go lua go_org_imports(1000) ]]
