97965f385d8cd85815e78811d7f1e30db0c25aae
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2022-08-26 03:02:35 +0900

    outside plugin config

---
+++ lua/p-conf/mason.lua
@@ -0,0 +1,17 @@
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers({
+    function(server_name) -- default handler (optional)
+        if server_name == "sumneko_lua" then
+            require("lspconfig")[server_name].setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+        else
+            require("lspconfig")[server_name].setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+        end
+    end,
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -67,85 +67,18 @@ require("packer").startup(function()
 end)
 vim.cmd([[autocmd BufWritePost init.lua source <afile> | PackerCompile]])
 
+require("nvim-autopairs").setup({
+    disable_filetype = { "TelescopePrompt", "vim" },
+})
+
 --style
 vim.cmd("colorscheme nightfox")
 require("lualine").setup()
 
 --lsp
+require("p-conf/cmp_nvim_lsp")
+require("p-conf/mason")
+require("p-conf/null-ls")
+require("p-conf.cmp")
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
-
-require("mason").setup()
-require("mason-lspconfig").setup()
-require("mason-lspconfig").setup_handlers({
-    function(server_name) -- default handler (optional)
-        if server_name == "sumneko_lua" then
-            require("lspconfig")[server_name].setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-            })
-        else
-            require("lspconfig")[server_name].setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-            })
-        end
-    end,
-})
-
-local null_ls = require("null-ls")
-local h = require("null-ls.helpers")
-null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.textlint.with({ filetypes = { "markdown" } }),
-    },
-})
-
-require("nvim-autopairs").setup({
-    disable_filetype = { "TelescopePrompt", "vim" },
-})
-
--- 3. completion (hrsh7th/nvim-cmp)
-local cmp = require("cmp")
-cmp.setup({
-    snippet = {
-        expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-        end,
-    },
-    sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        -- { name = "path" },
-    },
-    mapping = cmp.mapping.preset.insert({
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-l>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    }),
-    experimental = {
-        ghost_text = true,
-    },
-})
-function go_org_imports(wait_ms)
-    local params = vim.lsp.util.make_range_params()
-    params.context = { only = { "source.organizeImports" } }
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-    for _, res in pairs(result or {}) do
-        for _, r in pairs(res.result or {}) do
-            if r.edit then
-                vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
-            else
-                vim.lsp.buf.execute_command(r.command)
-            end
-        end
-    end
-end
-
-vim.cmd([[ autocmd BufWritePre *.go lua go_org_imports(1000) ]])
 --> lsp
