0eb0f286a222259db468ad7950e5102f2f2ec147
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2022-10-15 20:09:12 +0900

    fix update

--- lua/p-conf/mason.lua
+++ lua/p-conf/mason.lua
@@ -1,5 +1,5 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-lspconfig").setup_handlers({
