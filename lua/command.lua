23dcc9f5b6891f7db10c81839eefde9e5a27407c
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2023-02-05 18:27:39 +0900

    chenge Term cmd

--- lua/command.lua
+++ lua/command.lua
@@ -1,6 +1,5 @@
 -- Term and vertical
-vim.api.nvim_create_user_command("Term", function(opts)
+vim.api.nvim_create_user_command("VTerm", function(opts)
     vim.cmd("vsplit")
-    vim.cmd("wincmd l")
     vim.cmd("terminal")
 end, {})
