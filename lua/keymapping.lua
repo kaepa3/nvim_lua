09585c17d944aae3adbc4da20449c1f8765f46d5
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2023-08-23 23:23:02 +0900

    add git info + jj + exc

--- lua/keymapping.lua
+++ lua/keymapping.lua
@@ -7,6 +7,7 @@ lspinstaller.setup({})
 vim.keymap.set("n", "gn", ":bn<CR>")
 vim.keymap.set("n", "gp", ":bp<CR>")
 vim.keymap.set("n", "<ESC><ESC>", ":nohl<CR>")
+vim.keymap.set("i", "jj", "<Esc>")
 
 --general
 vim.keymap.set("i", "<C-o>", "<ESC>o")
