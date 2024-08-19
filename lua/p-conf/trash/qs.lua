1025ae8cfae18e9a6910e1df717ebcc8e6582d4b
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2024-08-20 01:29:04 +0900

    trash every

--- lua/p-conf/trash/qs.lua
+++
@@ -1,3 +0,0 @@
-vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
-vim.g.qs_max_chars = 150
-vim.g.qs_buftype_blacklist = { "terminal", "nofile", "figitive" }
--- snippets/trash/command.lua
+++
@@ -1,6 +0,0 @@
--- Term and vertical
-vim.api.nvim_create_user_command("VTerm", function(opts)
-    vim.cmd("vsplit")
-    vim.cmd("wincmd l")
-    vim.cmd("terminal")
-end, {})
