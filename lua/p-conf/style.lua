ddb25062bc45b7ba10e9bcada49634a73f868572
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2025-02-12 01:18:11 +0900

    transpalatint

--- lua/p-conf/style.lua
+++ lua/p-conf/style.lua
@@ -1,3 +1,21 @@
+vim.opt.termguicolors = true
+vim.opt.winblend = 0 -- ウィンドウの不透明度
+vim.opt.pumblend = 0 -- ポップアップメニューの不透明度
+
+require("nightfox").setup({
+    options = {
+        transparent = true,
+    },
+})
+require("tokyonight").setup({
+    options = {
+        transparent = true,
+        styles = {
+            sidebars = "transparent",
+            floats = "transparent",
+        },
+    }
+})
 require("styler").setup({
     themes = {
         help = { colorscheme = "tokyonight" },
@@ -5,7 +23,7 @@ require("styler").setup({
 })
 --colorchg
 BASE_COLORSCHEME = "nightfox"
-ACTIVE_COLORSCHEME = "nightfox"
+ACTIVE_COLORSCHEME = "tokyonight"
 INACTIVE_COLORSCHEME = "nordfox"
 
 local function inactivate(win)
