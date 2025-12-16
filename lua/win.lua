ed188c524b98c827c1b46f05f6be0213b6ff015b
TakahiroYamazaki <yamazaki@siriusvision.co.jp>
2025-12-16 14:05:55 +0900

    modified:   win.lua

--- lua/win.lua
+++ lua/win.lua
@@ -1,5 +1,2 @@
 vim.o.shell = "pwsh.exe"
 vim.o.shellcmdflag = "/c"
-vim.api.nvim_create_user_command("TermMSYS", function()
-    vim.cmd("terminal C:/msys64/msys2_shell.cmd -defterm -here -no-start -faststart")
-end, {})
