23888d398115420922e8f4f21371a37876c6d1ee
kaepa3 <kaepa3@gmail.com>
2026-08-03 23:06:10 +0900

    modified:   lua/p-conf/conform.lua

--- lua/p-conf/conform.lua
+++ lua/p-conf/conform.lua
@@ -1,20 +1,15 @@
 require("conform").setup({
     -- 保存時にフォーマットを実行する
     format_on_save = {
-        -- LSPのフォーマット機能が使える場合はそちらを優先する (任意)
-        lsp_fallback = true,
-        -- 同期的にフォーマットを実行（非同期だと保存タイミングとずれることがあるため）
-        async = false,
-        -- フォーマットがタイムアウトするまでの時間
+        -- LSPのフォーマットをフォールバックとして使用する
+        lsp_format = "fallback",
+        -- タイムアウト設定（この指定で自動的に同期実行になります）
         timeout_ms = 1000,
     },
 
     -- ファイルタイプごとのフォーマッタを指定
     formatters_by_ft = {
-        -- Vueファイルには 'prettier' を使用
         vue = { "prettier" },
-
-        -- 他のファイルタイプも同様に設定
         javascript = { "prettier" },
         typescript = { "prettier" },
         javascriptreact = { "prettier" },
